bd34797d2b2684bc419c76a709630445c9c7133f
Andrey <zaichenkoandrii322@gmail.com>
2022-06-22 17:10:35 +0300

    init

---
+++ App/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using static MonitoringLib.Recorder;
+using static MonitoringLib.ThreadInfo;
+
+Start();
+OutputThreadInfo();
+Console.WriteLine("Hello, World!");
+Stop();
---
+++ MonitoringLib/ThreadInfo.cs
@@ -0,0 +1,15 @@
+﻿using static System.Console;
+
+namespace MonitoringLib
+{
+    public static class ThreadInfo
+    {
+        public static void OutputThreadInfo()
+        {
+            Thread t = Thread.CurrentThread;
+            WriteLine("Thread Id: {0}, Priority: {1}, Background: {2}, Name: {3}",
+                t.ManagedThreadId, t.Priority,
+                t.IsBackground, t.Name ?? "null");
+        }
+    }
+}
