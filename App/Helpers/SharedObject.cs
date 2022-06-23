1a6729e085d664bbae9e35912450b47bd64326f8
Andrey <zaichenkoandrii322@gmail.com>
2022-06-23 16:50:04 +0300

    added new tests

---
+++ App/Helpers/SharedObject.cs
@@ -0,0 +1,104 @@
+﻿using MonitoringLib.ThreadMonitoring;
+using static System.Console;
+
+namespace App.Helpers
+{
+    internal class SharedObject
+    {
+        public string Message {
+            get => _message;
+            set => _message = value;
+        }
+
+        private readonly int _iterator;
+        private string _message;
+
+        public SharedObject(string message)
+        {
+            _message = message;
+            _iterator = 5;
+        }
+
+        public SharedObject() : this("") { }
+
+        public void WriteA()
+        {
+            for (int i = 0; i < _iterator; i++)
+            {
+                _message += "A";
+                ThreadInfo.OutputThreadInfo();
+            }
+            WriteLine();
+        }
+        
+        public void WriteB()
+        {
+            for (int i = 0; i < _iterator; i++)
+            {
+                _message += "B";
+                ThreadInfo.OutputThreadInfo();
+            }
+            WriteLine();
+        }
+
+        public void WriteAWithLock()
+        {
+            lock (this)
+            {
+                for (int i = 0; i < _iterator; i++)
+                {
+                    _message += "A";
+                    ThreadInfo.OutputThreadInfo();
+                }
+                WriteLine();
+            }
+        }
+
+        public void WriteBWithLock()
+        {
+            lock (this)
+            {
+                for (int i = 0; i < _iterator; i++)
+                {
+                    _message += "B";
+                    ThreadInfo.OutputThreadInfo();
+                }
+                WriteLine();
+            }
+        }
+
+        /// <summary>
+        /// Act the same way as WriteAWithLock function.
+        /// <br/>
+        /// Just to remember
+        /// </summary>
+        public void TestA()
+        {
+            /*
+             * When a thread calls Monitor.Enter on any object, aka reference type, it checks to see if some 
+             * other thread has already taken the conch. If it has, the thread waits. If it has not, the thread 
+             * takes the object and gets on with its work on the shared resource. Once the thread has finished 
+             * its work, it calls Monitor.Exit, releasing the object. If another thread was waiting, it can 
+             * now take the object and do its work
+             */
+            try
+            {
+                //To avoid deadlock need use Monitor.TryEnter
+                if (Monitor.TryEnter(this, TimeSpan.FromSeconds(15)))
+                {
+                    Monitor.Enter(this);
+                    for (int i = 0; i < _iterator; i++)
+                    {
+                        _message += "A";
+                        ThreadInfo.OutputThreadInfo();
+                    }
+                    WriteLine();
+                }
+            }
+            finally
+            {
+                Monitor.Exit(this);
+            }
+        }
+    }
+}
--- App/Program.cs
+++ App/Program.cs
@@ -1,21 +1,86 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using App.Helpers;
 using static MonitoringLib.ResourceMonitoring.Recorder;
 using static MonitoringLib.ThreadMonitoring.ThreadInfo;
 
-Start();
-Thread thread = new Thread(TestSimpleLoop);
-thread.Priority = ThreadPriority.Highest;
-thread.IsBackground = true;
-thread.Name = "MySecondThread";
-thread.Start();
-TestSimpleLoop();
-Console.WriteLine("End");
-Stop();
+Test2();
+
+void Test1()
+{
+    Start();
+
+    Thread currentThread = Thread.CurrentThread;
+    currentThread.Name = "MainThread";
+
+    Thread thread = new Thread(TestSimpleLoop);
+    thread.Priority = ThreadPriority.Highest;
+    thread.IsBackground = true;
+    thread.Name = "MySecondThread";
+
+    Thread parameterizedThread = new Thread(TestParameterizedThread);
+    parameterizedThread.Name = "ParameterizedThread";
+
+    thread.Start();
+    parameterizedThread.Start(100);
+    TestSimpleLoop();
+    
+    parameterizedThread.Join();
+    thread.Join();
+
+    Console.WriteLine("End");
+    Stop();
+}
+
+void Test2()
+{
+    Start();
+
+    var sharedObject = new SharedObject();
+
+    Task firstTask = Task.Run(sharedObject.WriteA);
+    Task secondTask = Task.Run(sharedObject.WriteB);
+
+    Console.WriteLine("FirstTest");
+    Task.WaitAll(new Task[] { firstTask, secondTask });
+    Console.WriteLine(sharedObject.Message);
+    sharedObject.Message = "";
+    Console.WriteLine();
+
+    Task firstTaskWithObjectLock = Task.Run(sharedObject.WriteAWithLock);
+    Task secondTaskWithObjectLock = Task.Run(sharedObject.WriteBWithLock);
+
+    Console.WriteLine("SecondTest");
+    Task.WaitAll(new Task[] { firstTaskWithObjectLock, secondTaskWithObjectLock });
+    Console.WriteLine(sharedObject.Message);
+
+    Console.WriteLine();
+    Stop();
+}
 
 void TestSimpleLoop()
 {
     for (int i = 0; i < 100; i++)
     {
         OutputThreadInfo();
+    }
+}
+
+void TestParameterizedThread(object? obj)
+{
+    
+    try
+    {
+        if (obj is null)
+            return;
+
+        int iterator = (int)obj;
+        for (int i = 0; i < iterator; i++)
+        {
+            OutputThreadInfo();
+        }
+    }
+    catch (InvalidCastException e)
+    {
+        throw new InvalidCastException("Error: Incorrect unboxing.", e);
     }
 }
